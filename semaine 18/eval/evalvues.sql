7654ec0e6fe86844a7f84c60f34ca9446a5efb11
mazuriermaxime <79094199+mazuriermaxime@users.noreply.github.com>
2021-05-07 15:49:37 +0200

    Delete evalvues.sql

--- semaine 18/eval/evalvues.sql
+++
@@ -1,7 +0,0 @@
---Créez une vue qui affiche le catalogue produits. L'id, la référence et le nom des produits, ainsi que l'id et le nom de la catégorie doivent apparaître.
-
-CREATE VIEW v_catalogue_produits
-AS
-SELECT pro_id,pro_ref,pro_name,cat_id,cat_name
-FROM products, categories
-WHERE products.pro_id = categories.cat_id
